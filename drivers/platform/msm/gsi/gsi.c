661143eab9fbf67aa5048325ae818c641f21bb4b
Martin <recordyletsplays@gmail.com>
2021-03-11 20:54:55 +0100

    msm: gsi: don't export gsi_pending_irq_type symbol

--- drivers/platform/msm/gsi/gsi.c
+++ drivers/platform/msm/gsi/gsi.c
@@ -2857,7 +2857,6 @@ int gsi_pending_irq_type(void)
 	return gsi_readl(gsi_ctx->base +
 		GSI_EE_n_CNTXT_TYPE_IRQ_OFFS(ee));
 }
-EXPORT_SYMBOL(gsi_pending_irq_type);
 
 int gsi_start_channel(unsigned long chan_hdl)
 {
